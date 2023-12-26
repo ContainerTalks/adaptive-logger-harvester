ec7d1f96c82354f8c123a7063e4b759fad6dcf37
JinnaBalu <jinna.balu@outlook.com>
2023-12-26 19:43:32 +0530

    created the docker-compose and ignore files

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 import os
+import time
 import docker
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import logging
 import re
+from dateutil import parser
 from elasticsearch import Elasticsearch
 
-# Set environment variables or provide default values
-CONTAINER_NAME_PATTERN = os.getenv("CONTAINER_NAME_PATTERN", "kibana")
-TIME_SINCE_IN_MIN = int(os.getenv("TIME_SINCE_IN_MIN", 20))
-ENV = os.getenv("ENV", "staging")
+CONTAINER_NAME_FILTER = os.getenv("CONTAINER_NAME_FILTER", "kibana")
+LOGS_TIME_OFFSET_MINUTES = int(os.getenv("LOGS_TIME_OFFSET_MINUTES", 20))
+APP_ENVIRONMENT = os.getenv("APP_APP_ENVIRONMENTIRONMENT", "staging")
 APP_NAME = os.getenv("APP_NAME", "kibana")
-INDEX_NAME = os.getenv("INDEX_NAME", "on-demand-logs")
+DEFAULT_INDEX_NAME = APP_NAME+"-"+"on-demand-logs"
+INDEX_NAME = os.getenv("INDEX_NAME", DEFAULT_INDEX_NAME)
 ELASTICSEARCH_URL = os.getenv("ELASTICSEARCH_URL", "https://localhost:9200")
-ELASTICSEARCH_CA_CERT = os.getenv("ELASTICSEARCH_CA_CERT", "/path/to/your/ca.crt")
+ELASTICSEARCH_CA_CERT = os.getenv("ELASTICSEARCH_CA_CERT", "/Volumes/project/Docker-X/elastic-stack/synonyms-search/certs/ca/ca.crt")
 ELASTICSEARCH_USER = os.getenv("ELASTICSEARCH_USER", "elastic")
 ELASTICSEARCH_PASSWORD = os.getenv("ELASTICSEARCH_PASSWORD", "elastic123")
 LAST_TIMESTAMP_FILE = os.getenv("LAST_TIMESTAMP_FILE", "last_timestamp.txt")
 
 def connect_to_elasticsearch():
     try:
-        client = Elasticsearch(
-            ELASTICSEARCH_URL,
-            ca_certs=ELASTICSEARCH_CA_CERT,
-            basic_auth=(ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD)
-        )
+        if ELASTICSEARCH_USER and ELASTICSEARCH_PASSWORD and ELASTICSEARCH_CA_CERT:
+            client = Elasticsearch(
+                ELASTICSEARCH_URL,
+                ca_certs=ELASTICSEARCH_CA_CERT,
+                basic_auth=(ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD)
+            )
+        else:
+            client = Elasticsearch(ELASTICSEARCH_URL)
+
         print(client.info())
         return client
     except Exception as e:
@@ -43,42 +49,30 @@ def index_logs(es, index_name, container_id, container_name, start_time, log_ent
     except Exception as e:
         logging.error(f"Error indexing logs: {e}")
 
-def read_last_timestamp():
-    try:
-        with open(LAST_TIMESTAMP_FILE, "r") as file:
-            return int(file.read())
-    except (FileNotFoundError, ValueError):
-        return None
-
-def write_last_timestamp(timestamp):
-    with open(LAST_TIMESTAMP_FILE, "w") as file:
-        file.write(str(timestamp))
 
 def retrieve_container_logs():
     try:
-        # Connect to Elasticsearch
         es = connect_to_elasticsearch()
-        
+
         if es:
             client = docker.from_env()
+            current_time = datetime.now(timezone.utc)
+            start_time = current_time - timedelta(minutes=LOGS_TIME_OFFSET_MINUTES)
 
-            current_time = datetime.now()
-            start_time = current_time - timedelta(minutes=TIME_SINCE_IN_MIN)
-            
             start_timestamp = int(start_time.timestamp())
-            
+
             containers = client.containers.list(all=True)
-            
+
             for container in containers:
                 container_name = container.attrs['Name'].lstrip('/')
-                
-                if CONTAINER_NAME_PATTERN.lower() in container_name.lower():
+
+                if CONTAINER_NAME_FILTER.lower() in container_name.lower():
                     container_id = container.id
+
                     logs = client.containers.get(container_id).logs(since=start_timestamp).decode('utf-8')
-                    
-                    # Combine multiline logs with the same timestamp into a single entry
+
                     log_entries = []
-                    current_log_entry = {"env": ENV, "app_name": APP_NAME, "message": ""}
+                    current_log_entry = {"env": APP_ENVIRONMENT, "app_name": APP_NAME, "message": ""}
                     timestamp_pattern = re.compile(r'\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}\]')
 
                     for line in logs.splitlines():
@@ -86,14 +80,16 @@ def retrieve_container_logs():
                             # Start a new log entry
                             if current_log_entry["message"]:
                                 log_entries.append(current_log_entry)
-                            current_log_entry = {"env": ENV, "app_name": APP_NAME, "message": line.strip()}
+                            current_log_entry = {"env": APP_ENVIRONMENT, "app_name": APP_NAME, "message": line.strip()}
                         else:
                             # Continue the current log entry
                             current_log_entry["message"] += f"\n{line.strip()}"
 
+                    # Check if the last log entry is not empty
+                    if current_log_entry["message"]:
+                        log_entries.append(current_log_entry)
+
                     # Index the log entries into Elasticsearch
-                    print(INDEX_NAME)
-                    print(log_entries)
                     index_logs(es, INDEX_NAME, container_id, container_name, start_time, log_entries)
 
     except docker.errors.APIError as e:
